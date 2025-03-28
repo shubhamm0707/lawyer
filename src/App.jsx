3f2e344b949beb8069a70de3c9b08be5a150812c
Skorax <skorax@Skoraxs-MacBook-Air.local>
2025-03-28 16:55:48 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,27 @@ import Article from "./Article";
 import Footer from "./Footer";
 import ExpertiseContainer from "./Expertise-container";
 import Banner from "./Banner";
-import data from "./data.json";
+import SeeMore from "./SeeMore";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [showMore, setShowMore] = useState(false);
+  const [data, setData] = useState([]);
+
+  // https://shubhamm0707.github.io/jsonAPI/data.json
+  useEffect(() => {
+    fetch("https://shubhamm0707.github.io/jsonAPI/data.json")
+      .then(response => response.json())
+      .then(data => {
+        console.log("work..");
+        console.log(data);
+        setData(data);
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <div>
       <Header />
@@ -13,10 +32,11 @@ function App() {
         <h1>BLOGS AND INSIGHTS</h1>
       </section>
       <div>
-        {data.map((item) => (
-          <Article cardTitle={item.cardTitle} borderColor={item.borderColor} imgUrl={item.imgUrl} />
-        ))}
+        {data && data.map((item) => {
+          return <Article cardTitle={item.cardTitle} borderColor={item.borderColor} imgUrl={item.imgUrl} />
+        })}
       </div>
+      {/* <SeeMore data={data} showMore={showMore} setShowMore={setShowMore} /> */}
       <ExpertiseContainer />
       <Footer />
     </div>
