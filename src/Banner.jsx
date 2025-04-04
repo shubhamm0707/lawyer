1cb4f8cb5b9c522679816cc95c3ff19c90664ddd
Skorax <skorax@Skoraxs-MacBook-Air.local>
2025-04-04 13:12:40 +0530

    first commit

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,72 +1,36 @@
-import { RiMenu3Line } from "react-icons/ri";
-import { ImCross } from "react-icons/im";
-import { useEffect, useState } from "react";
 import Header from "./Header";
 import './Banner.css';
+import Nav from "./Nav"
 
 
 
 function Banner() {
 
-    const [isOpen, setOpen] = useState(false);
-    const OpenMenu = () => {
-        setOpen(!isOpen);
-    }
-
     return <>
-        <div className="relative">
-            <div className={`w-[80vw] h-[100vh] transition-all duration-500 ease-in-out bg-orange-500 
-                            absolute top-0 ${isOpen ? 'right-[0vw]' : 'right-[-80vw]'} 
-                            flex items-start justify-center z-[9999]`}>
 
-                <div className="absolute mt-20 z-[-10] left-0">
-                    <div className="w-40 h-40 text-white text-2xl rounded-full flex cursor-pointer">
-                        <div className="relative w-full h-full bg-orange-500">
-
-                            <img src={process.env.PUBLIC_URL + "/hinge.png"} alt="Profile Picture" className="w-full h-full relative left-[-50%] filter-color-orange-500 object-cover rotate-[-90deg]" />
-                        </div>
-                        <RiMenu3Line className="relative left-[-100%]" onClick={OpenMenu} />
-                    </div>
-                </div>
-
-                <div className="flex flex-col items-center justify-center gap-6 mt-10 w-full">
-                    <h1 className="text-white text-[24px] font-bold">Mahima Chaudhary</h1>
-
-                    <div className="flex flex-col justify-center gap-6 w-[80%] mt-10 text-white">
-                        <button className="neuromorphic-btn">HOME</button>
-                        <button className="neuromorphic-btn">ABOUT ME</button>
-                        <button className="neuromorphic-btn">SERVICES</button>
-                        <button className="neuromorphic-btn">BLOGS</button>
-                        <button className="neuromorphic-btn">CONTACT ME</button>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
-
+        <Nav />
 
         <section className="banner flex flex-col justify-start">
 
-            <Header isMenuOpen={isOpen} />
+            <Header />
 
 
             <div className="bg-red-300 rounded-[50%] w-[60vw] h-[60vw] mt-20 md:w-[50vh] md:h-[50vh]">
-                <img className="rounded-[50%] w-full h-full object-cover object-top" src={process.env.PUBLIC_URL + "/bg.png"} alt="Profile Picture" />
+                <img className="rounded-[50%] w-full h-full object-cover" src={process.env.PUBLIC_URL + "/mahima.png"} alt="Profile Picture" />
             </div>
 
 
 
             <div className="text-center p-[10%] text-white">
-                <p className="text-[16px] font-bold">Hi, I am <span className="text-orange-500 text-[24px]">Mahima</span>, Corporate Lawyer | Executive - Compliance, Ethics & Investigations</p>
+                <p className="text-[16px] font-bold">Hi, I am <span className="text-white text-[24px]">Mahima</span>, Corporate Lawyer | CS Professional</p>
             </div>
 
             <div className="px-[10%] text-center text-gray-200 text-[12px]">
-                I am a corporate law professional with expertise in *corporate advisory, capital markets, infrastructure and project finance, and regulatory compliance*.
+                I am a corporate law professional with expertise in corporate advisory, capital markets, infrastructure and project finance, and regulatory compliance.
             </div>
 
 
-            <button className="primary-btn mt-14 w-[50%] h-[7%] bg-orange-500 text-[14px]">Download Resume</button>
+            <button className="primary-btn mt-14 w-[50%] h-[7%] bg-white text-[14px] text-[#004080]">Download Resume</button>
 
 
 
@@ -83,6 +47,7 @@ function Banner() {
                 <button className="sec-btn">Schedule a Consultation</button>
             </div>
         </section> */}
+
         </section>
     </>
 
